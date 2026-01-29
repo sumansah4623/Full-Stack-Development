df4209f0e5a91adc627c2d51004c634bec698fe0
suman kumari <sumansah4623@gmail.com>
2026-01-29 14:32:29 +0530

    App Router is Added

---
+++ Front-End/NextJs/Nextjs-Tutorial/my-nextjs-app/app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is my About Page.</p>
+        </div>
+    );
+}
---
+++ Front-End/NextJs/Nextjs-Tutorial/my-nextjs-app/app/page.js
@@ -0,0 +1,25 @@
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Hello World!</h1>
+//     </div>
+//   );
+// }
+
+
+
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+
+      <ul>
+        <li><Link href= "/about">About</Link></li>
+        <li><Link href= "/contact">Contact</Link></li>
+      </ul>
+    </div>
+  );
+}
