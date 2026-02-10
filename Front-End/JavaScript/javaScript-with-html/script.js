4ffc82b7763b1766f3f3432034083005fd09dea7
suman kumari <sumansah4623@gmail.com>
2026-02-10 16:03:14 +0530

    Added Database Folder

---
+++ Front-End/JavaScript/javaScript-with-html/script.js
@@ -0,0 +1,12 @@
+//printing 
+//console.log("Hello JavaScript World");
+
+
+
+//variable in Js
+fullName = "neha kakkar";
+age = 30;
+price = 99.99;
+x = null;
+y = undefined;
+console.log(fullName);
